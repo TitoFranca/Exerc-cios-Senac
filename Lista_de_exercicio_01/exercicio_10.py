465caeb2baa6c1aa68fd4c2cee5b2027cad4ef97
Seu NomeTitoAlves <tialvesfran@hotmail.com>
2026-04-12 17:13:17 -0300

    exercicio

--- Lista_de_exercicio_01/exercicio_10.py
+++ Lista_de_exercicio_01/exercicio_10.py
@@ -4,4 +4,4 @@ duas_vezes = valor_compra/2
 tres_vezes = valor_compra/3
 quatro_vezes = valor_compra/4
 cinco_vezes = valor_compra/5
-print(f"\nEm quantas vezes deseja pagar?:\n À vista {uma_vez:.2f}, 2x {duas_vezes:.2f}, 3x {tres_vezes:.2f}, 4x {quatro_vezes:.2f}; 5x {cinco_vezes:.2f}")
+print(f"\nEm quantas vezes deseja pagar?:\n \nÀ vista: R$ {uma_vez:.2f}\n2x: R$ {duas_vezes:.2f}\n3x: R$ {tres_vezes:.2f}\n4x: R$ {quatro_vezes:.2f}\n5x: R$ {cinco_vezes:.2f}\n \nMAIS QUE ISSO IMPOSSÍVEL!\nTÁ DURO? FICA EM CASA!")
