028f4e1f8a818998b7edcd48d29c0b7a8b30004c
Seu NomeTitoAlves <tialvesfran@hotmail.com>
2026-04-19 18:01:16 -0300

    aaa

--- aleatorio.py
+++ aleatorio.py
@@ -1,1 +1,4 @@
-print(2+"a")
+for i in range(4, 15):
+    for j in range(4, 15):
+        print(i, "*", j, "=", i * j, end="\t") # Print the equation
+    print() # Move to the next line after each row
---
+++ max_value.py
@@ -0,0 +1,6 @@
+max_value = 80 #determinar com variável para mudar apenas o valor da variável
+
+for i in range(max_value +1): #utilizar o +1 para utilizar o número da variável
+    if i  % 3 == 0 and i % 4 == 0: # i % 12 == 0 faz o mesmo resultado
+        print(f"{i} é divisível por 3 e por 4")
+    
