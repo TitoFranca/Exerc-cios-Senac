d101654bece005796c084bf018866c5a23275606
Seu NomeTitoAlves <tialvesfran@hotmail.com>
2026-04-20 12:32:44 -0300

    a

--- Aleatórios/max_value.py
+++ Aleatórios/max_value.py
@@ -1,4 +1,4 @@
-max_value = 80 #determinar com variável para mudar apenas o valor da variável
+max_value = 120 #determinar com variável para mudar apenas o valor da variável
 
 for i in range(max_value +1): #utilizar o +1 para utilizar o número da variável
     if i  % 3 == 0 and i % 4 == 0: # i % 12 == 0 faz o mesmo resultado
